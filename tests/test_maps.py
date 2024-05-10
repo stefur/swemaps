1a8ff8a68b98e5428e0233301373fac44c5e7bb2
Stefan Furne <stefan@furne.net>
2024-05-10 16:54:26 +0200

    Simple tests and workflow

---
+++ tests/test_maps.py
@@ -0,0 +1,45 @@
+"""Simple test for the GeoParquet files"""
+
+import geopandas as gpd  # type: ignore[import-untyped]
+import pyarrow.parquet as pq  # type: ignore[import-untyped]
+import swemaps
+from geopandas import GeoDataFrame
+from pyarrow import Table
+
+# Map types along with expected shapes
+MAP_TYPES = {
+    "lan": {"shape": (21, 3)},
+    "kommun": {"shape": (290, 3)},
+    "fa": {"shape": (60, 6)},
+}
+
+
+def test_geopandas():
+    """All of the map types should be able to load in GeoPandas"""
+    for map_type in MAP_TYPES:
+        shape = MAP_TYPES[map_type]["shape"]
+        path = swemaps.get_path(map_type)
+        gdf = gpd.read_parquet(path)
+        assert isinstance(gdf, GeoDataFrame)
+        assert gdf.shape == shape
+
+
+def test_pyarrow():
+    """All of the map types should be able to load in PyArrow"""
+    for map_type in MAP_TYPES:
+        shape = MAP_TYPES[map_type]["shape"]
+        tbl = pq.read_table(swemaps.get_path(map_type))
+        assert isinstance(tbl, Table)
+        assert tbl.shape == shape
+        geojson = swemaps.pyarrow_to_geojson(tbl)
+        features_length = len(geojson["features"])
+        unpacked_properties = [
+            list(prop["properties"].keys()) for prop in geojson["features"]
+        ]
+        # Unpack inner lists and keep only distinct values
+        properties_length = len(
+            list(set(item for sublist in unpacked_properties for item in sublist))
+        )
+
+        # +1 for geometries
+        assert (features_length, properties_length + 1) == shape
