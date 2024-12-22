2289743e5e504a74df6dfdfe56ef064b35f163ac
Stefan Furne <stefan@furne.net>
2024-12-22 09:45:27 +0100

    Improved tests

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -1,61 +1,51 @@
 """Simple test for the GeoParquet files"""
 
-import json
-
-import geopandas as gpd  # type: ignore[import-untyped]
-import pyarrow.compute as pc  # type: ignore[import-untyped]
-import pyarrow.parquet as pq  # type: ignore[import-untyped]
+import geopandas as gpd
+import pyarrow.parquet as pq
+import pytest
 from geopandas import GeoDataFrame
 from pyarrow import Table
 
 import swemaps
 
-# Map types along with expected shapes
-MAP_TYPES: dict[swemaps.utils.BuiltinMap, dict] = {
-    "lan": {"shape": (21, 3)},
-    "kommun": {"shape": (290, 3)},
-    "fa": {"shape": (60, 6)},
-}
+map_shapes = [("lan", (21, 3)), ("kommun", (290, 3)), ("fa", (60, 6))]
 
 
-def test_geopandas():
+@pytest.mark.parametrize("map_type, shape", map_shapes)
+def test_geopandas(map_type, shape):
     """All of the map types should be able to load in GeoPandas"""
-    for map_type in MAP_TYPES:
-        shape = MAP_TYPES[map_type]["shape"]
-        path = swemaps.get_path(map_type)
-        gdf = gpd.read_parquet(path)
-        assert isinstance(gdf, GeoDataFrame)
-        assert gdf.shape == shape
+    path = swemaps.get_path(map_type)
+    gdf = gpd.read_parquet(path)
+    assert isinstance(gdf, GeoDataFrame)
+    assert gdf.shape == shape
 
 
-def test_pyarrow():
+@pytest.mark.parametrize("map_type, shape", map_shapes)
+def test_pyarrow(map_type, shape):
     """All of the map types should be able to be loaded as PyArrow Tables and be converted to GeoJSON"""
-    for map_type in MAP_TYPES:
-        shape = MAP_TYPES[map_type]["shape"]
-        tbl = pq.read_table(swemaps.get_path(map_type))
-        assert isinstance(tbl, Table)
-        assert tbl.shape == shape
-        geojson = swemaps.table_to_geojson(tbl)
-        features_length = len(geojson["features"])
-        unpacked_properties = [
-            list(prop["properties"].keys()) for prop in geojson["features"]
-        ]
-        # Unpack inner lists and keep only distinct values
-        properties_length = len(
-            list(set(item for sublist in unpacked_properties for item in sublist))
-        )
 
-        # +1 for geometries
-        assert (features_length, properties_length + 1) == shape
+    tbl = pq.read_table(swemaps.get_path(map_type))
+    assert isinstance(tbl, Table)
+    assert tbl.shape == shape
+    geojson = swemaps.table_to_geojson(tbl)
+    features_length = len(geojson["features"])
+    unpacked_properties = [
+        list(prop["properties"].keys()) for prop in geojson["features"]
+    ]
+    # Unpack inner lists and keep only distinct values
+    properties_length = len(
+        list(set(item for sublist in unpacked_properties for item in sublist))
+    )
+
+    # +1 for geometries
+    assert (features_length, properties_length + 1) == shape
 
 
-def test_geojson():
+@pytest.mark.parametrize("map_type", ["lan", "kommun", "fa"])
+def test_geojson(snapshot, map_type):
     """GeoJSON output should match expected"""
-    tbl = pq.read_table(swemaps.get_path("lan")).filter(pc.field("lan_kod") == "01")
+    tbl = pq.read_table(swemaps.get_path(map_type))
 
     geojson = swemaps.table_to_geojson(tbl)
 
-    with open("tests/sthlm.geojson", mode="r", encoding="utf-8") as file:
-        expected = json.load(file)
-
-    assert geojson == expected
+    assert geojson == snapshot
